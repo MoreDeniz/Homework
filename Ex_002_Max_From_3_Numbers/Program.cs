ddf474b75e7191c146aec203dfa3f79eaf5ff519
Mayya <msalosina@mail.ru>
2022-10-14 12:31:39 +0700

    add Ex 002 Max From 3 Numbers

---
+++ Ex_002_Max_From_3_Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿//Нахождения максимального числа из трёх введённых
+Console.Clear();
+Console.Write("Введите первое число: ");
+int numb_1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numb_2 = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int numb_3 = int.Parse(Console.ReadLine());
+int max = numb_1;
+
+if (numb_2 > max)
+{
+    max = numb_2;
+}
+if (numb_3 > max)
+{
+    max = numb_3;
+}
+    Console.WriteLine($"Максимум: число {max}");
+
