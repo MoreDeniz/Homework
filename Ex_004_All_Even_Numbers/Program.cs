a51f7bded22cc27408da83b26e307964e500ee2c
Mayya <msalosina@mail.ru>
2022-10-14 13:50:41 +0700

    add Ex 004 All Even Numbers

---
+++ Ex_004_All_Even_Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿// Вывод всех чётных чисел от 1 до введённого
+Console.Clear();
+Console.Write("Введите число: ");
+int numb = int.Parse(Console.ReadLine());
+int even = 2;
+
+if (numb < even)
+{
+    Console.WriteLine($"Таких чисел нет");
+}
+else
+    while (even <= numb)
+    {
+        Console.Write($"{even}; ");
+        even += 2;
+    }
+Console.WriteLine();
