999476eb637839e43c23d77478c51fefdff0c6bc
Mayya <msalosina@mail.ru>
2022-10-14 13:07:55 +0700

    add Ex 003 If Even Number

---
+++ Ex_003_If_Even_Numb/Program.cs
@@ -0,0 +1,19 @@
+﻿// Проверка числа на чётность
+Console.Clear();
+Console.Write("Введите число: ");
+int numb = int.Parse(Console.ReadLine());
+
+if (numb == 0)
+{
+    Console.WriteLine($"Это число 0");
+}
+else if (numb % 2 != 0)
+{
+    Console.WriteLine($"Число {numb} нечётное");
+}
+else
+{
+    Console.WriteLine($"Число {numb} чётное");
+}
+
+
