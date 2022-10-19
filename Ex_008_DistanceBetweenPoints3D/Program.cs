435f821b3fed5673341803f8d9725b4e14d109fb
Mayya <msalosina@mail.ru>
2022-10-19 20:44:12 +0700

    delited checking 3D

--- Ex_008_DistanceBetweenPoints3D/Program.cs
+++ Ex_008_DistanceBetweenPoints3D/Program.cs
@@ -4,8 +4,8 @@ Console.Clear();
 double EnterNumber()    //function: Enter Coordinates
 {
     Console.Write("Enter coordinate: ");
-double num = double.Parse(Console.ReadLine() ?? "0");
-return num;
+    double num = double.Parse(Console.ReadLine() ?? "0");
+    return num;
 }
 
 double Diff(double num1, double num2)   //function: difference of two numbers
@@ -28,7 +28,7 @@ double SquareNumber(double num)   //function: square of a number
 
 double DistanceBetweenPoints(double num)   //function: Square Root of a number
 {
-    double distance =  Math.Sqrt(num);
+    double distance = Math.Sqrt(num);
     return distance;
 }
 
@@ -40,16 +40,6 @@ Console.WriteLine("Enter coordinates X, Y, Z of the Second Point:");
 double x2 = EnterNumber();
 double y2 = EnterNumber();
 double z2 = EnterNumber();
-
-Console.WriteLine(Diff(x1, x2)); //checking for difference
-Console.WriteLine(Diff(y1, y2));
-Console.WriteLine(Diff(z1, z2));
-
-Console.WriteLine(SquareNumber(Diff(x1, x2)));  //checking for square
-Console.WriteLine(SquareNumber(Diff(y1, y2)));
-Console.WriteLine(SquareNumber(Diff(z1, z2)));
-
-Console.WriteLine(Sum3(SquareNumber(Diff(x1, x2)), SquareNumber(Diff(y1, y2)), SquareNumber(Diff(z1, z2))));  //checking for sum
 
 Console.Write($"Distance between Points: A({x1}, {y1}, {z1}) and B({x2}, {y2}, {z2}) is ");
 Console.WriteLine(DistanceBetweenPoints(Sum3(SquareNumber(Diff(x1, x2)), SquareNumber(Diff(y1, y2)), SquareNumber(Diff(z1, z2)))));
