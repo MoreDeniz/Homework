d305ea84aecfab75aa5890cb9db14fae5b4f1317
Mayya <msalosina@mail.ru>
2022-10-19 14:31:11 +0700

    add Function Enter Number

--- Ex_006_Is_a_Sqare/Program.cs
+++ Ex_006_Is_a_Sqare/Program.cs
@@ -1,7 +1,14 @@
 ﻿//2. По двум заданным числам проверять является ли одно квадратом другого
 Console.Clear();
 
-int SquareNumber(int num)
+int EnterNumber()    //function Enter Number
+{
+    Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine() ?? "0");
+return num;
+}
+
+int SquareNumber(int num)   //function calculate the square of a number
 {
     int square = num * num;
     return square;
@@ -11,11 +18,8 @@ bool IsSquare(int num1, int num2)
 {
     return num1 == SquareNumber(num2);
 }
+int num1 = EnterNumber();
+int num2= EnterNumber();
 
-Console.Write("Введите число: ");
-int num1 = int.Parse(Console.ReadLine() ?? "0");
-Console.Write("Введите число: ");
-int num2 = int.Parse(Console.ReadLine() ?? "0");
-
-Console.WriteLine(IsSquare(num1, num2) ? $"Число {num1} квадрат числа {num2}" : $"Число {num1} не квадрат числа {num2}");
-Console.WriteLine(IsSquare(num2, num1) ? $"Число {num2} квадрат числа {num1}" : $"Число {num2} не квадрат числа {num1}");
+Console.WriteLine(IsSquare(num1, num2) ? $"Число {num1} квадрат числа {num2};" : $"Число {num1} не квадрат числа {num2};");
+Console.WriteLine(IsSquare(num2, num1) ? $"Число {num2} квадрат числа {num1};" : $"Число {num2} не квадрат числа {num1};");
