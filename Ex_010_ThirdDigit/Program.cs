d69aa4762bb0b13d938cc91afd94b236cf2a8b38
Mayya <msalosina@mail.ru>
2022-10-20 19:45:18 +0700

    add Ex 010 Third Digit

---
+++ Ex_010_ThirdDigit/Program.cs
@@ -0,0 +1,11 @@
+﻿// Вывод третьей цифры введённого числа
+Console.Clear();
+
+Console.Write("Enter the number: ");
+string num = Console.ReadLine(); //Введённое число в виде строки
+
+if (int.Parse(num[0].ToString()) != 0)
+    Console.WriteLine((num.Length < 3)
+    ? $"There is no third digit in the number {num}."
+    : $"The third digit in the number {num} --> {int.Parse(num[2].ToString())}");
+else Console.WriteLine($"Entered incorrectly!");
