e912df2cb46c6bb797a9cf6df9e8a83cc56acb0a
Mayya <msalosina@mail.ru>
2022-10-20 19:20:21 +0700

    add Ex 009 Second Digit

---
+++ Ex_009_SecondDigit/Program.cs
@@ -0,0 +1,9 @@
+﻿﻿// Выводит вторую цифру 3-значного числа
+Console.Clear();
+Console.Write("Enter a three-digit number: ");
+int num = int.Parse(Console.ReadLine());
+int secondDigit = (num / 10) % 10;
+
+Console.WriteLine((num > 99 && num < 1000) 
+? $"The second digit in the number {num} = {secondDigit}." 
+: $"Entered incorrectly!");
