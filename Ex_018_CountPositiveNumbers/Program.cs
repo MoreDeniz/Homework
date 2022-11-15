3d03607931edf4bc0cc8ede7fcf8acd8bec8aa4e
Mayya <msalosina@mail.ru>
2022-11-15 22:23:58 +0700

    add Ex_018 Count Positive Numbers

---
+++ Ex_018_CountPositiveNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.  
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+
+double[] GetArrayOfNumbers()
+{
+    Console.WriteLine("Введите числа через пробел: ");
+    string userMassiv = Console.ReadLine();
+
+    double[] numbers = userMassiv.Split(' ').Select(double.Parse).ToArray();
+  
+    return numbers;
+}
+
+int CountPositiveNumbers(double[] array)
+{
+    int posNumbers = 0;
+    foreach (double el in array)
+    {
+        if (el > 0) posNumbers++;
+    }
+    Console.WriteLine($"Положительных чисел {posNumbers}.");
+    return posNumbers;
+}
+
+Console.WriteLine();
+CountPositiveNumbers(GetArrayOfNumbers());
