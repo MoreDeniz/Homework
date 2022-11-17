2743b4fd89f58bc9f4288cbb6002d363c6fbbe89
Mayya <msalosina@mail.ru>
2022-11-18 01:10:02 +0700

    add Ex 020 2D Real Array

---
+++ Ex_020_2DRealArray/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n,
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Clear();
+
+int InputNumber(string message)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool result = int.TryParse(Console.ReadLine() ?? "0", out int number);
+        if (!result)
+        {
+            Console.WriteLine($"Введены некорректные данные!");
+            Thread.Sleep(1000);
+
+            continue;
+        }
+        return number;
+    }
+}
+
+double[,] GetArrayOfRealNumbers(int m, int n)
+{
+    double[,] array = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = Math.Round(new Random().NextDouble() + new Random().Next(-100,100), 1);  //* 100, 1);
+        }
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = InputNumber("Введите число строк:    m = ");
+int n = InputNumber("Введите число столбцов: n = ");
+PrintArray(GetArrayOfRealNumbers(m, n));
+
