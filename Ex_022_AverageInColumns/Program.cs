94fe59fb62f7d257cd9bc8be960aa03384fe1ec9
Mayya <msalosina@mail.ru>
2022-11-19 11:55:04 +0700

    add Ex 022 Average In Columns

---
+++ Ex_022_AverageInColumns/Program.cs
@@ -0,0 +1,84 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Clear();
+
+int InputNumber(string message)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool result = int.TryParse(Console.ReadLine() ?? "0", out int number);
+        if (!result)
+        {
+            Console.WriteLine($"Ввод некорректный! Попробуйте ещё раз.");
+            Thread.Sleep(1500);
+            continue;
+        }
+        return number;
+    }
+}
+
+int[,] GetMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}   ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+double[] AverageColumns(int[,] array)
+{
+    double[] average = new double[array.GetLength(1)];
+
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[i, j];
+            average[j] = Math.Round(sum / array.GetLength(0), 1);
+        }
+    }
+    return average;
+}
+
+
+int m = InputNumber($"Введите количество строк:    ");
+int n = InputNumber($"Введите количество столбцов: ");
+int[,] matrix = GetMatrix(m, n);
+PrintMatrix(matrix);
+double[] array = AverageColumns(matrix);
+Console.WriteLine();
+PrintArray(array);
+Console.WriteLine();
