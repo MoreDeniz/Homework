e90d8917875573faab1879351183eddd25461a45
Mayya <msalosina@mail.ru>
2022-11-21 21:28:20 +0700

    add Ex 023 Descending Elements Of The Rows

---
+++ Ex_023_DescendingElementsOfTheRows/Program.cs
@@ -0,0 +1,80 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу,
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+Console.Clear();
+
+int InputNumber(string message)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool result = int.TryParse(Console.ReadLine() ?? "0", out int number);
+        if (!result)
+        {
+            Console.WriteLine($"Ввод некорректный! Попробуйте ещё раз.");
+            Thread.Sleep(1500);
+            continue;
+        }
+        return number;
+    }
+}
+
+int[,] GetMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}   ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void DescendingElementsOfTheRows(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            int maxPosition = j;
+            for (int k = j + 1; k < matrix.GetLength(1); k++)
+            {
+                if (matrix[i,k] > matrix[i,maxPosition]) maxPosition = k;
+            }
+            int temp = matrix[i,j];
+            matrix[i,j] = matrix[i,maxPosition];
+            matrix[i,maxPosition] = temp;
+        }
+    }
+}
+
+int m = InputNumber("Задайте количество строк массива:    ");
+int n = InputNumber("Задайте количество столбцов массива: ");
+int[,] matrix = GetMatrix(m, n);
+Console.WriteLine();
+PrintMatrix(GetMatrix(m, n));
+Console.WriteLine();
+DescendingElementsOfTheRows(matrix);
+PrintMatrix(matrix);
