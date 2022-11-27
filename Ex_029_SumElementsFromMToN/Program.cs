5eacceffee0842b4e953942312b98ccc7634a26d
Mayya <msalosina@mail.ru>
2022-11-27 22:44:09 +0700

    add Ex 029 Sum Elements From M To N

---
+++ Ex_029_SumElementsFromMToN/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая 
+// найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Clear();
+
+int InputNumber(string message)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool result = int.TryParse(Console.ReadLine() ?? "0", out int number);
+        if (!result)
+        {
+            Console.WriteLine($"Ввод некорректный! Попробуйте ещё раз.");
+            Thread.Sleep(1500);
+            continue;
+        }
+        return number;
+    }
+}
+
+int SumElements(int min, int max)
+{
+    if (min == max) return min;
+
+    return min + SumElements(min+1, max);
+}
+
+int m = InputNumber("Задайте число: ");
+int n = InputNumber("Задайте число: ");
+Console.WriteLine($"Сумма элементов от {m} до {n} равна {SumElements(m, n)}");
