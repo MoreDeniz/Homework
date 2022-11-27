e46114199c31a8b22ae93c492c7a95f35a26202e
Mayya <msalosina@mail.ru>
2022-11-27 22:33:13 +0700

    add Ex 030 Akkermana Function

---
+++ Ex_030_AkkermanaFunction/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.Clear();
+
+uint InputNumber(string message)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool result = uint.TryParse(Console.ReadLine() ?? "0", out uint number);
+        if (!result)
+        {
+            Console.WriteLine($"Ввод некорректный! Попробуйте ещё раз.");
+            Thread.Sleep(1500);
+            continue;
+        }
+        return number;
+    }
+}
+
+// функция Аккермана
+uint A(uint n, uint m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return A(n - 1, 1);
+    else
+      return A(n - 1, A(n, m - 1));
+}
+
+uint n = InputNumber("Задайте число: ");
+uint m = InputNumber("Задайте число: ");
+Console.WriteLine($"A(n, m) = {A(n, m)}");
